1c2441e82140acbe44ae3329b510808eeedb4a6c
Vashti <patrena@sbcglobal.net>
2021-10-06 21:20:01 -0700

    initial react code

---
+++ src/components/Flashcard/index.js
@@ -0,0 +1,22 @@
+import React,{ useState } from 'react'
+
+
+export default function Flashcard({flashcard}) {
+    const[flip, setFlip]=useState(false)
+    return (
+        <div 
+    className={`card ${flip ? 'flip' :''}`}
+        onClick={()=> setFlip(!flip)}
+        >
+    <div className ="front">
+        {flashcard.service}
+        
+           </div>
+           <div className="back">
+              {flashcard.description}
+            <div className="flashcard-price">{flashcard.price}</div> 
+           </div>
+          {/* {flip ? flashcard.service : flashcard.description}  */}
+        </div>
+    )
+}
---
+++ src/pages/Services.js
@@ -0,0 +1,16 @@
+import react from "react";
+// import { Carousel } from "react-bootstrap";
+// import Hero from "../components/Hero/index.js";
+// import BootstrapCarouselComponent from '../components/Carousel/index.js';
+
+
+function Services(props) {
+  return (
+      <div>
+          is working
+ {/* <Hero title={props.title} subTitle={props.subTitle} subscript={props.subscript}/>
+ <BootstrapCarouselComponent></BootstrapCarouselComponent> */}
+</div>
+  );
+}
+export default Services;
