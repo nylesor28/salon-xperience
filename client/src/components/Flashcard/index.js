96cc40ef1f4c1855ad8f14fa2b93c8997ee8e2df
Vashti <patrena@sbcglobal.net>
2021-10-19 20:38:36 -0700

    pricing list and shadow to cards, adjusted card size

--- client/src/components/Flashcard/index.js
+++ client/src/components/Flashcard/index.js
@@ -8,11 +8,31 @@ export default function Flashcard({flashcard}) {
     className={`card ${flip ? 'flip' :''}`}
         onClick={()=> setFlip(!flip)}
         >
-    <div className ="front">
+    <div className ="front"style ={{
+            //    backgroundColor: "white",
+               height: 200,
+               padding: 0,
+               width: 200,
+               color: "silver",
+               display: "flex",
+               flexDirection: "column",
+               justifyContent: "center",
+               alignItems: "center",
+           }}>
         {flashcard.service}
         
            </div>
-           <div className="back">
+           <div className="back" style ={{
+               backgroundColor: "white",
+               height: 200,
+               padding: 0,
+               width: 200,
+               color: "silver",
+               display: "flex",
+               flexDirection: "column",
+               justifyContent: "center",
+               alignItems: "center",
+           }}>
               {flashcard.description}
             <div className="flashcard-price">{flashcard.price}</div> 
            </div>
