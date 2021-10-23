13d81590ab04dde63a55e902d35676f81979909f
Vashti <patrena@sbcglobal.net>
2021-10-22 18:29:30 -0700

    updated seeds.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,42 +2,72 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
-const Order = require('./Order');
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, 'Must match an email address!']
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5
+    },
+    userProfile: 
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'UserProfile'
+      }
+    ,
+    role: 
+      {
+        type: String,
+        enum : ['client', 'stylist', 'admin'],
+        default: "client"
+      }
+    
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5
-  },
-  orders: [Order.schema]
-});
+
+);
 
 // set up pre-save middleware to create password
 userSchema.pre('save', async function(next) {
+  console.log("========SAVE========")
+  console.log("this.is new : " , this.isNew)
+  console.log("this.is mod : " , this.isModified('password'))
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
+    console.log("========HASHED SAVE========")
+    console.log(this)
   }
 
   next();
 });
 
+userSchema.pre('findByIdAndUpdate', async function(next) {
+  console.log("========FIND AND UPDATE ========")
+  console.log("this.is new : " , this.isNew)
+  console.log("this.is mod : " , this.isModified('password'))
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    console.log("========HASH FIND AND UPDATE========")
+    console.log(this)
+  }
+
+  next();
+});
 // compare the incoming password with the hashed password
 userSchema.methods.isCorrectPassword = async function(password) {
   return await bcrypt.compare(password, this.password);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,29 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type UserProfile {
+  _id: ID
+  firstName: String!
+  lastName: String!
+  phoneNumber: String!
+  address: String
+  city: String
+  zipCode: String
+  imageURL : String
+}
+input UserProfileInput {
+  firstName: String!
+  lastName: String!
+  phoneNumber: String!
+  address: String
+  city: String
+  zipCode: String
+  imageURL : String
+}
   type Service {
     _id: ID
     name: String
   }
-
   type Product {
     _id: ID
     name: String
@@ -15,31 +33,28 @@ const typeDefs = gql`
     price: Float
     service: Service
   }
-
+  type User {
+    _id: ID
+    userName: String!
+    password: String!
+    email: String!
+    role: String
+    userProfile: UserProfile
+  }
   type Order {
     _id: ID
     purchaseDate: String
     products: [Product]
   }
-
-  type User {
-    _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    orders: [Order]
-  }
-
   type Checkout {
     session: ID
   }
-
   type Auth {
-    token: ID
+    token: ID!
     user: User
   }
-
   type Query {
+    getUserProfile : User
     services: [Service]
     products(service: ID, name: String): [Product]
     product(_id: ID!): Product
@@ -47,14 +62,17 @@ const typeDefs = gql`
     order(_id: ID!): Order
     checkout(products: [ID]!): Checkout
   }
-
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateProduct(_id: ID!, quantity: Int!): Product
+    addUpdateUserProfile( profileInput: UserProfileInput!) : User
+    addUser( username: String!, email: String!, password: String!, profileId: ID, role: String): Auth
     login(email: String!, password: String!): Auth
+    updateUser(firstName: String, lastName: String, email: String ): User
+    updatePassword(oldPassword: String, newPassword: String): User
+    addOrder(products: [ID]!): Order
+   
+    updateProduct(_id: ID!, quantity: Int!): Product
   }
 `;
 
 module.exports = typeDefs;
+
