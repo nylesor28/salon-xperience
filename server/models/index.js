ecf67f92abb740c6c1c7230b96b288a3e9d415f8
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-24 22:54:25 -0400

    add stylist model info/and gql query and mutation

--- server/models/index.js
+++ server/models/index.js
@@ -5,5 +5,6 @@ const User = require('./User');
 const Product = require('./Product');
 const Service = require('./Service');
 const Order = require('./Order');
+const Stylist = require('./Stylist');
 
-module.exports = { User, Product, Service, Order,UserProfile , Client};
+module.exports = { User, Product, Service, Order,UserProfile , Client, Stylist};
