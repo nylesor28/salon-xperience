296b4db04c1529a7e99dbb448a549e60c9da7045
Vashti <patrena@sbcglobal.net>
2021-10-25 10:32:26 -0700

    background image

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -57,6 +57,51 @@ class BootstrapCarouselComponent extends React.Component {
                     </p>
                   </Carousel.Caption>
                 </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={erikmclean}
+                    alt="Third slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl
+                      consectetur.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={erikmclean}
+                    alt="Third slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl
+                      consectetur.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                 <Carousel.Item>
+                  <img
+                    className="d-block w-100"
+                    src={erikmclean}
+                    alt="Third slide"
+                  />
+
+                  <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl
+                      consectetur.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
               </Carousel>
             </div>
           </div>
