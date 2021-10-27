faee28436e97983dcc14ffe6c4cffb86c9f49d96
Akeem C <javantieclarke@gmail.com>
2021-10-27 14:06:36 -0400

    updated the admin page

--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -1,21 +1,46 @@
 import React from "react";
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ALL_STYLISTS} from "../../utils/queries"
+import { useQuery, useMutation } from "@apollo/client";
+import { QUERY_ALL_SERVICES } from "../../utils/queries";
 function UserList() {
-  const {data: userData} = useQuery(GET_ALL_STYLISTS);
-  const fullData = userData;;
+  const { data: userData } = useQuery(QUERY_ALL_SERVICES);
+  const fullData = userData?.getAllServices;
 
-// console.log(fullData.getAllStylists);
+  console.log(fullData);
   return (
     <>
-      <div className="w-3/12 bg-white opacity-90 p-2 shadow-lg rounded-lg">
-         <h2 className="text-center font-bold text-pink-600"> All Active Clients</h2>
-            <div className="border rounded-lg p-2">
-             <span>Username:<p> Akeem</p></span>
-             <span>Username:<p> Akeem</p></span>
-             <span>Username:<p> Akeem</p></span>
-             <span>Username:<p> Akeem</p></span>
+      <div className="w-3/12 bg-white opacity-95 p-2 shadow-lg rounded-lg">
+        <h2 className="text-center font-bold text-pink-600">
+          {" "}
+          All Active Clients
+        </h2>
+        {fullData &&
+          fullData.map((service) => (
+            <div className="border-2 rounded-lg p-1">
+              <div
+                id="wrapper"
+                className="border pl-4 pt-1 rounded shadow opacity-80"
+              >
+                <div className="flex ">
+                  <p className="font-bold"> Service Name: </p>
+                  <p className="pl-4"> {service.serviceName}</p>
+                </div>
+                <div className="flex ">
+                  <p className="font-bold"> Service Price: </p>
+                  <p className="pl-4"> ${service.price}</p>
+                </div>
+                <div className="flex ">
+                  <p className="font-bold"> Duration: </p>
+                  <p className="pl-4">
+                    {" "}
+                    {service.duration.hour} HOUR {service.duration.minute} MINS
+                  </p>
+                </div>
+                <button className="bg-red-400 text-white font-bold p-1 mb-2 rounded mt-2 border-2 shadow hover:bg-red-900">
+                  Delete Service
+                </button>
+              </div>
             </div>
+          ))}
       </div>
     </>
   );
