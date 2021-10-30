8567b516c577429d4438f36ed6e17da8d4b531ad
Vashti <patrena@sbcglobal.net>
2021-10-30 07:19:21 -0700

    stylist update

--- client/src/pages/Stylist.js
+++ client/src/pages/Stylist.js
@@ -33,10 +33,10 @@ function Stylist(props) {
 const StylistFlashcards=[{
     id: 0,
     stylist:"Roz",
-    speciality: "natural Hair",
-    education:"Empire Beauty School",
+    speciality: "Speciality: Natural Hair",
+    education:"Education: Empire Beauty School",
     image: img1,
-    description: "long term",
+    description: "BIO:I’m Roz, and I’ve been a Colorist at Salon Xperience for the past five years and has over 10 years of color and stylist experience. The wildest color I’ve ever dyed her hair was lime green and it was my favorite!",
     ratings:"Ratings:⭐⭐⭐⭐⭐"
 },
 {
@@ -51,37 +51,37 @@ const StylistFlashcards=[{
 {
   id: 2,
   stylist:"Emilia",
-  speciality: "natural Hair",
-  education:"Empire Beauty School",
+  speciality: "Speciality: Custom Color, Formal Styling, and hair types",
+  education:"Education: Empire Beauty School",
   image: img3,
-  description: "long term",
+  description: "BIO: Emilia has been a Colorist at Salon Xperience for the past eight years and has ten years of color and styling experience. She joined the Salon team in our mentorship program upon obtaining her license. After two years of working under our Master Colorist, Emilia has matured into full Colorist and now specializes in unique highlighting techniques, such as balayage and “baby highlights.”",
   ratings:"Ratings:⭐⭐⭐⭐⭐"
 },
 {
   id: 3,
   stylist:"Eva",
-  speciality: "natural Hair",
-  education:"Empire Beauty School",
+  speciality: "Speciality: Design, Color, and Natural Hair",
+  education:"Education: Empire Beauty School",
   image: img4,
-  description: "long term",
+  description: "BIO: Eva is a barber who has worked at Salon Xperience for the past five years. She specializes in straight razor shaves for those who like a more traditional shave. Eva creates a relaxed environment for her clients and looks forward to making you look and feel your best.",
   ratings:"Ratings:⭐⭐⭐⭐⭐"
 },
 {
   id: 4,
   stylist:"Jurnee",
-  speciality: "natural Hair",
-  education:"Paul Mitchell School",
+  speciality: "Speciality: custom color and natural Hair",
+  education:"Education: Paul Mitchell School",
   image: img5,
-  description: "long term",
+  description: "BIO: Jurnee has been a Master Colorist at Salon for the past five years and has over 15 years of color and styling experience. Jurnee is trained and certified in multiple color systems and enjoys using her knowledge to create exactly what her clients are envisioning when they sit down in her chair.",
   ratings:"Ratings:⭐⭐⭐⭐⭐"
 },
 {
   id: 5,
   stylist:"Maya",
-  speciality: "natural Hair",
-  education:"Paul Mitchell School",
+  speciality: "Speciality: natural Hair",
+  education:"Education: Paul Mitchell School",
   image: img6,
-  description: "long term",
+  description: "BIO: Maya has been a Colorist at Salon Xperience for the past eight years and has ten years of color and styling experience. She joined the Salon Xperience team in our mentorship program upon obtaining her license. After two years of working under Jurnee our Master Colorist, Maya became a full Colorist and now specializes in unique highlighting techniques, such as balayage and “baby highlights.”",
   ratings:"Ratings:⭐⭐⭐⭐⭐"
 }
 ]
